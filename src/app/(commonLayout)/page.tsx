32ee112d20ac8ccbacb028e23fec78658719538c
PallabKumarS <pallabkumar26@gmail.com>
2025-02-13 11:41:25 +0600

    started working on a new practice project with nextjs

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div className="container mx-auto mt-4">
+      <h1>This is HomePage Component</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default HomePage;
